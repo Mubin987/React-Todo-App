cc4200d2940927773836f7137263ee20ecab3a80
Mubin987 <99292441+Mubin987@users.noreply.github.com>
2023-06-13 01:15:02 +0500

    edit functionality added

--- src/pages/todo1.jsx
+++ src/pages/todo1.jsx
@@ -2,20 +2,30 @@ import {Link} from "react-router-dom"
 import { useState , useContext } from "react"
 import Todocontext from "../context/todo/TodoContext"
 import '../App.css'
+import { BiEdit } from "react-icons/bi";
 // import TodoProvider from "../context/todo/TodoProvider"
 const Todo = () => {
     const [InputValue,setInputValue] = useState('')
+    const [togglebtn,settogglebtn] = useState(false)
+    const [indexlocal,setindexlocal] = useState(-1)
     const {Todos,setTodo} = useContext(Todocontext)
+    const buttonStyle = {
+        border:"0px",
+        padding:"10px",
+        margin:"0px"
+    }
     const AddTodo = () => {
     //    var t = document.getElementById('todos')
     //    var node = document.createElement('div')
     //    node.textContent = InputValue
-        setTodo([...Todos,InputValue])
+    if(InputValue!=''){
+        setTodo([...Todos,InputValue])}
     //    t.appendChild(node)
         setInputValue('')
     }
     const HandleInput = (getval) => {
         setInputValue(getval.target.value)
+        console.log(Todos)
     }
     // const restore = () => {
     //     return (Todos.map((item) => {
@@ -29,16 +39,36 @@ const Todo = () => {
           return updatedTodos;
         });
       };
+    const EditTodo = (index) => {
+        settogglebtn(true)
+        setindexlocal(index)
+    }
+    const AddEditedTodo = () => {
+        if(InputValue!=''){
+            setTodo((prevtodos) => {
+                const editedtodo = [...prevtodos]
+                editedtodo[indexlocal] = InputValue
+                return editedtodo
+            })
+        }
+        setInputValue('')
+        settogglebtn(false)
+    }
     return(
         <div>
             <Link to='/' className="link-style">Goto Home</Link>
             <h1>Todo 1</h1>
-            <input type="text" onChange={HandleInput} value={InputValue} />
-            <button onClick={AddTodo}>Add Todo</button>
+            {
+                togglebtn ? <input type="text" placeholder={`Edit to-do ${indexlocal + 1} here`}  onChange={HandleInput} value={InputValue} /> : <input type="text" placeholder="Add new to-do here" onChange={HandleInput} value={InputValue} />
+            }
+            
+            {
+                togglebtn ? <button onClick={AddEditedTodo}>Edit Todo</button> : <button onClick={AddTodo}>Add Todo</button>
+            }
             <button onClick={RemoveTodo}>Remove Todo</button>
             {/* <button onClick={restore}>Restore list</button> */}
             <div id="todos">
-                {Todos.map((item) => {return <p>{item}</p>;}) }
+                {Todos.map((item,index) => {return <p><pre>{item}   <button style={buttonStyle} onClick={() => EditTodo(index)}><BiEdit /></button></pre></p>;}) }
             </div>
         </div>
     )
--- src/pages/todo2.jsx
+++ src/pages/todo2.jsx
@@ -2,26 +2,26 @@ import {Link} from "react-router-dom"
 import { useState , useContext } from "react"
 import Todocontext2 from "../context/todo2/TodoContext2"
 import '../App.css'
-// import TodoProvider from "../context/todo/TodoProvider"
-const Todo2 = () => {
+import { BiEdit } from "react-icons/bi";
+const Todo = () => {
     const [InputValue,setInputValue] = useState('')
+    const [togglebtn,settogglebtn] = useState(false)
+    const [indexlocal,setindexlocal] = useState(-1)
     const {Todos2,setTodo2} = useContext(Todocontext2)
+    const buttonStyle = {
+        border:"0px",
+        padding:"10px",
+        margin:"0px"
+    }
     const AddTodo = () => {
-    //    var t = document.getElementById('todos')
-    //    var node = document.createElement('div')
-    //    node.textContent = InputValue
-        setTodo2([...Todos2,InputValue])
-    //    t.appendChild(node)
+    if(InputValue!=''){
+        setTodo2([...Todos2,InputValue])}
         setInputValue('')
     }
     const HandleInput = (getval) => {
         setInputValue(getval.target.value)
+        console.log(Todos2)
     }
-    // const restore = () => {
-    //     return (Todos.map((item) => {
-    //       return <p key={item}>{item}</p>;
-    //     }))
-    // }
     const RemoveTodo = () => {
         setTodo2((prevTodos) => {
           const updatedTodos = [...prevTodos];
@@ -29,19 +29,38 @@ const Todo2 = () => {
           return updatedTodos;
         });
       };
+    const EditTodo = (index) => {
+        settogglebtn(true)
+        setindexlocal(index)
+    }
+    const AddEditedTodo = () => {
+        if(InputValue!=''){
+            setTodo2((prevtodos) => {
+                const editedtodo = [...prevtodos]
+                editedtodo[indexlocal] = InputValue
+                return editedtodo
+            })
+        }
+        setInputValue('')
+        settogglebtn(false)
+    }
     return(
         <div>
             <Link to='/' className="link-style">Goto Home</Link>
             <h1>Todo 2</h1>
-            <input type="text" onChange={HandleInput} value={InputValue} />
-            <button onClick={AddTodo}>Add Todo</button>
+            {
+                togglebtn ? <input type="text" placeholder={`Edit to-do ${indexlocal + 1} here`}  onChange={HandleInput} value={InputValue} /> : <input type="text" placeholder="Add new to-do here" onChange={HandleInput} value={InputValue} />
+            }
+            
+            {
+                togglebtn ? <button onClick={AddEditedTodo}>Edit Todo</button> : <button onClick={AddTodo}>Add Todo</button>
+            }
             <button onClick={RemoveTodo}>Remove Todo</button>
-            {/* <button onClick={restore}>Restore list</button> */}
             <div id="todos">
-                {Todos2.map((item) => {return <p>{item}</p>;}) }
+                {Todos2.map((item,index) => {return <p><pre>{item}   <button style={buttonStyle} onClick={() => EditTodo(index)}><BiEdit /></button></pre></p>;}) }
             </div>
         </div>
     )
 }
 
-export default Todo2
+export default Todo
